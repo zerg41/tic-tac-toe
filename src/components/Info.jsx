ed06777993d68033bfe6d5b18a0fe88a404d92d8
zerg41 <zerg41@gmail.com>
2022-10-08 17:58:53 +0300

    wip updated project structure; moved moves history info to Log component

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,38 +1,27 @@
-import React from "react";
+import React from 'react';
 // utils
-import { calculateWinner } from "../utils/util";
+import { calculateWinner } from '../utils/util';
 
+function Info({ history, stepNumber, xIsNext }) {
+  const MAX_STEPS = 9;
+  let currentMove = history[stepNumber];
+  let winner = calculateWinner(currentMove.squares);
+  let status;
 
-function Info({ history, stepNumber, handleMove, xIsNext }) {
+  if (winner) {
+    status = `Winner is: ${winner.id}`;
+  } else if (stepNumber === MAX_STEPS) {
+    status = `Draw`;
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
 
-    const currentMove = history[stepNumber];
-    const movesList = history.map((step, move) => {
-        const description = move ? `Go To Move #${move}` : `Go To Game Start`;
-
-        return (
-            <li key={move}>
-                <button onClick={() => handleMove(move)}>{description}</button>
-            </li>
-        );
-    });
-
-    let status;
-    const winner = calculateWinner(currentMove.squares);
-
-    if (winner) {
-        status = `Winner is: ${winner.id}`;
-    } else if (stepNumber === 9) {
-        status = `Draw`;
-    } else {
-        status = `Next player: ${xIsNext ? 'X' : 'O'}`;
-    }
-
-    return (
-        <div className="game-info">
-            <div>{status}</div>
-            <ol>{movesList}</ol>
-        </div>
-    );
+  return (
+    <div className='info'>
+      <h2>Game Status</h2>
+      <div>{status}</div>
+    </div>
+  );
 }
 
 export default Info;
